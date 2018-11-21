8eb8b1febdc15fc5d0efcf9bebf13edf2a6654f1
Silas <silogecho@gmail.com>
2018-11-21 14:40:14 +0300

    Guess number

--- assignment1/scripts/Publish.py
+++ assignment1/scripts/Publish.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python
 
+import random
 import rospy
 from std_msgs.msg import String
+from std_msgs.msg import Int32
 
 def talker():
-    pub = rospy.Publisher('chatter', String, queue_size=10)
+    # pub = rospy.Publisher('chatter', String, queue_size=10)
+    pub = rospy.Publisher('Guess', Int32, queue_size=10)
     rospy.init_node('talker', anonymous=True)
-    rate = rospy.Rate(100) # 10hz
+    rate = rospy.Rate(1) # 10hz
     while not rospy.is_shutdown():
-        hello_str = "Hi Robot how are u %s" % rospy.get_time()
-        rospy.loginfo(hello_str)
-        pub.publish(hello_str)
+        # hello_str = "Hi Robot guess this %s" % rospy.get_time()
+        num = random.randint(1,5)
+        rospy.loginfo("My Number is: %d", num)
+        # pub.publish(hello_str)
+        pub.publish(num)
         rate.sleep()
 
 if __name__ == '__main__':
--- assignment1/scripts/listener.py
+++ assignment1/scripts/listener.py
@@ -1,16 +1,25 @@
 #!/usr/bin/env python
-
+import random
 import rospy
 from std_msgs.msg import String
+from std_msgs.msg import Int32
 
 def callback(data):
-    rospy.loginfo(rospy.get_caller_id() + 'I heard u ROBO %s', data.data)
+    my_guess = random.randint(1,5)
+    num1= data.data
+    rospy.loginfo("my numner %d",num1)
+    if(my_guess == num1):
+        rospy.loginfo('Yaaa ... You got it')
+    else:
+        rospy.loginfo("You loss")
 
 def listener():
 
     rospy.init_node('listener', anonymous=True)
 
-    rospy.Subscriber('chatter', String, callback)
+    rospy.Subscriber('Guess', Int32, callback)
+
+    # rospy.Subscriber('position', Int32, callback1)
     rospy.spin()
 
 if __name__ == '__main__':
